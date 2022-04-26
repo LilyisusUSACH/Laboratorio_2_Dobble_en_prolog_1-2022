fe39422220ceb97d7bf67861f111cb9ddc0eb992
LilyisusUSACH <alejandro.cortes.c@usach.cl>
2022-04-26 16:22:19 -0400

    [Medio Avance] Creacion de archivos, TDA's y documentacion por defecto, todo sujeto a modificaciones

---
+++ labDobble_20722036_CortesCortez.pl
@@ -0,0 +1,112 @@
+%%%%%%%%%%%%%
+%% TDA CARD
+%%%%%%%%%%%%%
+% Dominios
+
+% Predicados
+
+% Metas
+    % Secundarias
+
+    % Primarias
+
+% Clausulas
+
+    % Hechos
+
+    % Reglas
+
+%%%%%%%%%%%%%
+%% TDA CARDS
+%%%%%%%%%%%%%
+
+% Dominios
+
+% Predicados
+
+% Metas
+    % Secundarias
+
+    % Primarias
+
+% Clausulas
+
+    % Hechos
+
+    % Reglas
+
+%%%%%%%%%%%%%
+%% TDA CARDSET
+%%%%%%%%%%%%%
+
+% Dominios
+
+% Predicados
+
+% Metas
+    % Secundarias
+
+    % Primarias
+
+% Clausulas
+
+    % Hechos
+
+    % Reglas
+
+%%%%%%%%%%%%%
+%% TDA Player
+%%%%%%%%%%%%%
+
+% Dominios
+
+% Predicados
+
+% Metas
+    % Secundarias
+
+    % Primarias
+
+% Clausulas
+
+    % Hechos
+
+    % Reglas
+
+%%%%%%%%%%%%%
+%% TDA Players
+%%%%%%%%%%%%%
+
+% Dominios
+
+% Predicados
+
+% Metas
+    % Secundarias
+
+    % Primarias
+
+% Clausulas
+
+    % Hechos
+
+    % Reglas
+
+%%%%%%%%%%%%%
+%% TDA GAME
+%%%%%%%%%%%%%
+
+% Dominios
+
+% Predicados
+
+% Metas
+    % Secundarias
+
+    % Primarias
+
+% Clausulas
+
+    % Hechos
+
+    % Reglas
